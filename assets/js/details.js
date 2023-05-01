68c9f9e150e0be53526817d792151ad65cd25b4b
LinusWeb <linooow@gmail.com>
2023-05-01 18:18:12 -0300

    exibindo detalhes de cada pokemon em uma nova pagina

--- assets/js/details.js
+++ assets/js/details.js
@@ -4,8 +4,33 @@ const maxRecords = 1
 const limit = 1
 let offset = 0;
 
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+window.onload = function() {
+    console.log("Carregou");
+    selectPokemon();
+};
+
+const selectPokemon = async (id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${postId}`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    convertPokemonToLi(pokemon)
+    console.log(pokemon);
+}
+
 function convertPokemonToLi(pokemon) {
-    return `<div class="pokemon ${pokemon.type}">
+
+    const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+ 
+    const photo = pokemon.sprites.other.dream_world.front_default
+    const htmlString =
+        `   <div class="pokemon ${pokemon.type}">
                 <div class="button">
                     <a href="javascript:window.history.back();"><img src="/assets/image/left-arrow.png"/></a>
                     <img src="/assets/image/heart.png"/>
@@ -15,7 +40,7 @@ function convertPokemonToLi(pokemon) {
                     <span class="name">${pokemon.name}</span>
                 </div>
                 <div class="number">
-                    <span class="number">#${pokemon.number}</span>
+                    <span class="number">#${pokemon.id}</span>
                 </div>            
                 
                 <div class="detail">
@@ -24,10 +49,11 @@ function convertPokemonToLi(pokemon) {
                     </ol>
                 </div>
                 <div class="image">
-                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    <img src="${photo}" alt="${pokemon.name}">
                 </div>
             </div>
-    `
+        `
+    pokemonList.innerHTML = htmlString + pokemonList.innerHTML
 }
 
 function loadPokemonItens(offset, limit) {
